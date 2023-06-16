0d5055c95ea9562461342b24e05dc96cf0923a7f
salss33 <hallosalsa03@gmail.com>
2023-06-16 18:11:09 +0700

    nambah elemen di stack

--- Stack_Array_189/Stack_Array_189.cpp
+++ Stack_Array_189/Stack_Array_189.cpp
@@ -12,4 +12,18 @@ public:
 	StackArray() {
 		top = -1;
 	}
+	void push() {
+		cout << "\nEnter an Element: ";
+		int element;
+		cin >> element;
+		if (top == 4) { //step 1
+			cout << "Number of data exceeds the limit." << endl;
+			return;
+		}
 
+		top++;
+		stack_array[top] = element; //step3
+		cout << endl;
+		cout << element << "ditambahkan(pushed)" << endl;
+
+	}
