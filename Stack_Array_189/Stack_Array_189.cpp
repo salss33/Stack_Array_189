7e2037f72d339f113a908c44b027de920a0ea1c5
salss33 <hallosalsa03@gmail.com>
2023-06-16 18:43:18 +0700

    buat ngehapus elemen teratas dari stack

--- Stack_Array_189/Stack_Array_189.cpp
+++ Stack_Array_189/Stack_Array_189.cpp
@@ -27,10 +27,12 @@ public:
 		cout << element << "ditambahkan(pushed)" << endl;
 
 	}
+	void pop() {
+		if (empty()) { //step 1
+			cout << "\nStack is empty. cannot pop." << endl; //1.a
+			return; //1.b
+		}
 
-
-
-
-
-
-
+		cout << "\nThe popped element is: " << stack_array[top] << endl; //step 2
+		top--; //step 3 decrement
+	}
