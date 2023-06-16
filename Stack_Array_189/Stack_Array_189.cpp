ea57050eac7fe84c235d9686be54cb53bc0bd329
salss33 <hallosalsa03@gmail.com>
2023-06-16 18:00:59 +0700

    kelas Stack Array

---
+++ Stack_Array_189/Stack_Array_189.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class StackArray {
+private:
+	int stack_array[5];
+	int top;
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
+	
